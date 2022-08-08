c8c4bc0702ac8a8e0b6c5fa50de16a9b4606c7ad
AshishK-dev <110808977+AshishK-dev@users.noreply.github.com>
2022-08-08 15:40:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Job_suggestions from "./components/joblists/joblists";
 import Searchbar from "./components/jobsearch/search_bar/searchbar";
 import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/Profile/profile";
-import Company_Signup from "./components/rec_signup/rec_signup";
+import User_Signup from "./components/signup/signup";
 import IntialInfo from "./components/registeration_process/basicDetails/basicDetails";
 import Emp_info from "./components/registeration_process/employment/employment";
 import EduInfo from "./components/registeration_process/education/educaion";
@@ -25,7 +25,7 @@ const App = () => {
       <Route path="/company_signup" element={<Company_Signup/>}/>
       <Route path="/initial_info" element={<IntialInfo/>}/>
       <Route path="/profile" element={<Profile/>}/>
-      <Route path="/signup" element={<Company_Signup/>}/>
+      <Route path="/signup" element={<User_Signup/>}/>
       <Route path="/info" element={<IntialInfo/>}/>
       <Route path="/emp_info" element={<Emp_info/>}/>
       <Route path="/EduInfo" element={<EduInfo/>}/>
